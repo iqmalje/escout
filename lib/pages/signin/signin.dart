63c80e954a12bf8d92b42e671f02aa4ca0ef5a93
FahmiRoslee <amiroslee20@gmail.com>
2023-11-06 19:51:05 +0800

    Added button functionality on sign in page

--- lib/pages/signin/signin.dart
+++ lib/pages/signin/signin.dart
@@ -11,130 +11,151 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.sizeOf(context).width,
-        height: MediaQuery.sizeOf(context).height,
-        margin: EdgeInsets.only(top: MediaQuery.sizeOf(context).height * 0.3),
-        decoration: const ShapeDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(0.00, -1.00),
-            end: Alignment(0, 1),
-            colors: [Color(0xFF3B3F65), Color(0xFF9254D8)],
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(45),
-              topRight: Radius.circular(45),
-            ),
-          ),
-        ),
-        //The space of the widget for the left edge in the container
-        padding: EdgeInsets.only(
-            left: MediaQuery.sizeOf(context).width * 0.15,
-            right: MediaQuery.sizeOf(context).width * 0.15),
-        // All widget Column
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            //Welcome and log in column
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Welcome to eScout',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    height: 0,
-                  ),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Text(
-                  'Log In',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w700,
-                    height: 0,
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(
-              height: 40,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    hintText: 'Email',
-                    filled: true, // Fill the background with color
-                    fillColor:
-                        Colors.white, // Set the background color to white
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    hintText: 'Password',
-                    filled: true, // Fill the background with color
-                    fillColor:
-                        Colors.white, // Set the background color to white
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                const Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Color(0xFFFFC600),
-                      fontSize: 12,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w500,
-                      decoration: TextDecoration.underline,
-                      height: 0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(
-              height: 40,
-            ),
+            SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.35,
+                child: Image.asset('assets/Escout Logo.png')),
             Container(
-              width: 300,
-              height: 50,
-              decoration: ShapeDecoration(
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 0.65,
+
+              decoration: const ShapeDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.00, -1.00),
+                  end: Alignment(0, 1),
+                  colors: [Color(0xFF3B3F65), Color(0xFF9254D8)],
+                ),
                 shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 3, color: Color(0xFFFFC600)),
-                  borderRadius: BorderRadius.circular(15),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(45),
+                    topRight: Radius.circular(45),
+                  ),
                 ),
               ),
-            )
+              //The space of the widget for the left edge in the container
+              padding: EdgeInsets.only(
+                  left: MediaQuery.sizeOf(context).width * 0.15,
+                  right: MediaQuery.sizeOf(context).width * 0.15),
+              // All widget Column
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Welcome and log in column
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'Welcome to eScout',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                          height: 0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                          height: 0,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          hintText: 'Email',
+                          filled: true, // Fill the background with color
+                          fillColor:
+                              Colors.white, // Set the background color to white
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          hintText: 'Password',
+                          filled: true, // Fill the background with color
+                          fillColor:
+                              Colors.white, // Set the background color to white
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      const Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Color(0xFFFFC600),
+                            fontSize: 12,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                            decoration: TextDecoration.underline,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(15),
+                      splashColor: Colors.red,
+                      onTap: () {
+                        print('tekan');
+                      },
+                      child: Ink(
+                        width: 300,
+                        height: 50,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                width: 3, color: Color(0xFFFFC600)),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
