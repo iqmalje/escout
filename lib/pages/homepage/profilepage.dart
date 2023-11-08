679fcaa8c6b409fcb6a5083e147e1055f2daaffd
Fahmi Roslee <amiroslee20@gmail.com>
2023-11-08 21:12:46 +0800

    profile page

--- lib/pages/homepage/profilepage.dart
+++ lib/pages/homepage/profilepage.dart
@@ -16,11 +16,59 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           children: [
             Container(
-          width: 390,
-          height: 110,
-          decoration: BoxDecoration(color: Color(0xFF2E3B78)),
-) 
-          ]),
+              width: MediaQuery.sizeOf(context).width,
+              height: 110,
+              decoration: const BoxDecoration(color: Color(0xFF2E3B78)),
+              child: const Center(
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 24,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            Container(
+              width: 330,
+              height: 40,
+              decoration: ShapeDecoration(
+                color: Color(0xFF2C225B),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              child: const Center(
+                child: Text(
+                  'Admin View',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w900,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            const Text(
+              'JOHOR SCOUT DIGITAL ID',
+              style: TextStyle(
+                fontSize: 12,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w900,
+                height: 0,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
